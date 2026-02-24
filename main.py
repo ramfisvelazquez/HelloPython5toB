afaaafb71e3651737526507553b737ad38017845
Ramfis Velazques <ramfisvelazquez47@gmail.com>
2026-02-24 12:39:37 -0400

    proyecto base de python

---
+++ main.py
@@ -0,0 +1,10 @@
+import pyfiglet
+
+nombre = input("ingrese su nombre:")
+banner = pyfiglet.figlet_format(nombre, font="slant")
+print("\n"+ "="*50 + "\n")
+
+print (banner)
+
+print ("\n"+ "="*50 + "\n")
+
